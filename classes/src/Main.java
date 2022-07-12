bdb64fee4648d2da9826070c63718474a9902be5
Diego <diegom.develops@gmail.com>
2022-07-12 11:51:01 -0400

    feat: Constant Variables and static
    Learning constant variables

--- classes/src/Main.java
+++ classes/src/Main.java
@@ -1,15 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Hello.helloWorld();
-        Doctor maria = new Doctor("Maria");
-        maria.showId();
-        Doctor said = new Doctor("Said");
-        said.showId();
-    }
-    static class Hello {
-        public static void helloWorld() {
-            System.out.println("Hello, world!");
-            return;
-        }
+    UIMenu.showMenu();
+
     }
 }
